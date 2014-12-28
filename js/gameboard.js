7a9cce462a80de610032dbc70fb4a3583c3c21ad
Paul Boone <narcissus.pong@gmail.com>
2014-12-28 12:30:37 -0800

    working drag and drop using angular  and interactjs

--- js/gameboard.js
+++ js/gameboard.js
@@ -1,8 +1,27 @@
 var gameboardApp = angular.module('gameboardApp', [])
 
 gameboardApp.controller('gameboardCtrl', function ($scope) {
+  
   $scope.stacks = [
     {'x':100,'y':100,'cards':['island','island','swamp','orgg']},
     {'x':300,'y':100,'cards':['orgg']}
   ]
+  
+  $scope.stackOnMove = function(event) {
+    $scope.stacks[event.target.dataset.index].x += event.dx
+    $scope.stacks[event.target.dataset.index].y += event.dy
+    $scope.$apply()
+  }
+  
+  interact('.draggable')
+    .draggable({
+      restrict: {
+        restriction: "parent",
+        endOnly: true,
+        elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+      },
+      onmove: $scope.stackOnMove
+    })  
 })
+
+  
