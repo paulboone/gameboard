8c99f46cc21c16b8022d28f035261c10ef959c79
Paul Boone <narcissus.pong@gmail.com>
2014-12-28 13:00:43 -0800

    added card rotation

--- js/gameboard.js
+++ js/gameboard.js
@@ -3,13 +3,25 @@ var gameboardApp = angular.module('gameboardApp', [])
 gameboardApp.controller('gameboardCtrl', function ($scope) {
   
   $scope.stacks = [
-    {'x':100,'y':100,'cards':['island','island','swamp','orgg']},
-    {'x':300,'y':100,'cards':['orgg']}
+    {'x':100,'y':100,'cards':['island','island','swamp','orgg'], 'rotation':0},
+    {'x':300,'y':100,'cards':['orgg'],'rotation':0}
   ]
   
   $scope.stackOnMove = function(event) {
-    $scope.stacks[event.target.dataset.index].x += event.dx
-    $scope.stacks[event.target.dataset.index].y += event.dy
+    var stack = $scope.stacks[event.target.dataset.index]
+    stack.x += event.dx
+    stack.y += event.dy
+    $scope.$apply()
+  }
+  
+  $scope.stackOnTap = function(event) {
+    var stack = $scope.stacks[event.currentTarget.dataset.index]
+
+    if (stack.rotation == 0) {
+      stack.rotation = 90
+    } else {
+      stack.rotation = 0
+    }
     $scope.$apply()
   }
   
@@ -21,7 +33,8 @@ gameboardApp.controller('gameboardCtrl', function ($scope) {
         elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
       },
       onmove: $scope.stackOnMove
-    })  
+    })
+    .on('tap',$scope.stackOnTap)
 })
 
   
